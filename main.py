ec66bd78dc103d88944765dae41013c15b46a2f1
punPPdev <phatai.punnathon@gmail.com>
2026-03-02 05:48:23 +0700

    add route get-movie-tv-show-data and store model on cache

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from typing import List, Optional
 from contextlib import asynccontextmanager
 import mlflow
 import pandas as pd
+from pandas import DataFrame
 
 
 mlflow_pyfunc_model = None
@@ -26,6 +27,23 @@ async def lifespan(app: FastAPI):
         print(f"Error loading model: {e}")
         
     yield
+
+
+def extract() -> DataFrame:
+    movie_df = pd.read_csv('netflix_movies_detailed_up_to_2025.csv')
+    tv_show_df = pd.read_csv('netflix_tv_shows_detailed_up_to_2025.csv')
+    concat_tv_movie = pd.concat([movie_df, tv_show_df], axis=0, ignore_index=True)
+    concat_tv_movie['director'] = concat_tv_movie['director'].fillna('unknown')
+    concat_tv_movie['cast'] = concat_tv_movie['cast'].fillna('unknown')
+    concat_tv_movie['country'] = concat_tv_movie['country'].fillna('unknown')
+    concat_tv_movie['duration'] = concat_tv_movie['duration'].fillna(0)
+    concat_tv_movie['genres'] = concat_tv_movie['genres'].fillna('unknown')
+    concat_tv_movie['description'] = concat_tv_movie['description'].fillna('unknown')
+    concat_tv_movie['budget'] = concat_tv_movie['budget'].fillna(0.0)
+    concat_tv_movie['revenue'] = concat_tv_movie['revenue'].fillna(0.0)
+    return concat_tv_movie
+
+cache_movie_tv_df = extract()
 
 #----------------------------------- Request Schema Layer -----------------------------------#
 class RecommendationRequest(BaseModel):
@@ -52,6 +70,23 @@ def health():
         'status': 'ok'
     }
     
+@app.get('/get-movie-tv-show-data')
+def get_movie_tv_show_data():
+    genres = cache_movie_tv_df['genres'].str.split(', ').explode('cast').unique().tolist()
+    title = cache_movie_tv_df['title'].unique().tolist()
+    cast = cache_movie_tv_df['cast'].str.split(', ').explode('cast').unique().tolist()
+    director = cache_movie_tv_df['director'].unique().tolist()
+    
+    return {
+        "status": "success",
+        "dashboard_data": {
+            'genres': genres,
+            'title': title,
+            'cast': cast,
+            'director': director
+        }
+    }
+    
 @app.post("/recommend")
 def get_recommendations(request: RecommendationRequest):
     """
@@ -70,7 +105,7 @@ def get_recommendations(request: RecommendationRequest):
     prediction_df = mlflow_pyfunc_model.predict(input_df)
 
     # Convert the resulting DataFrame back to JSON for the API response
-    return {"status": "SUCCESS", "recommendations": prediction_df.to_dict(orient="records")}
+    return {"status": "success", "recommendations": prediction_df.to_dict(orient="records")}
     
 if __name__ == '__main__':
     import uvicorn
