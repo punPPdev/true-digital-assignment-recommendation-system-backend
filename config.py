5d3d8b6ff651bf52fc1413e9eafd466ee8dc379d
punPPdev <phatai.punnathon@gmail.com>
2026-03-02 05:50:32 +0700

    set MLFLOW_URL constant value

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+import os
+
 title = 'Netflix Recommendation System'
 description = 'An API for netflix recommendation system'
 app_version = '0.0.1'
 
-MLFLOW_URL = "http://localhost:5000"
+
+
+MLFLOW_URL = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
 MLFOW_EXPERIMENT_NAME = "prod/content-base-recommender-model-v0.1"
 MLFLOW_RUN_NAME = "cosine-similarity-model"
 ML_FLOW_MODEL_NAME = 'content-base-recommender-model'
